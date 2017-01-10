725d775672e8c40517bc8086ba34d25e74973739
antonio-carvalho <acarvalho@veniamworks.com>
2017-01-10 07:28:34 +0000

    fixe-size_data: Finished finite state worlds (door simulation)

--- fixed-size_data/intervals_enumerations_itemizations/finite_state_worlds.rkt
+++ fixed-size_data/intervals_enumerations_itemizations/finite_state_worlds.rkt
@@ -101,19 +101,50 @@
 
 
 ; ------------------------------------------------------------------------------
+; Exercise 62
 (define LOCKED "locked")
 (define CLOSED "closed")
 (define OPEN "open")
 
 ; DoorState -> DoorState
 ; closes an open door over the period of one tick
-(check-expect (door-close LOCKED) LOCKED)
-(check-expect (door-close CLOSED) CLOSED)
-(check-expect (door-close OPEN) CLOSED)
-
+(check-expect (door-closer LOCKED) LOCKED)
+(check-expect (door-closer CLOSED) CLOSED)
+(check-expect (door-closer OPEN) CLOSED)
 (define (door-closer state-of-door)
   (cond
     [(string=? LOCKED state-of-door) LOCKED]
     [(string=? CLOSED state-of-door) CLOSED]
     [(string=? OPEN state-of-door) CLOSED]))
 
+; DoorState KeyEvent -> DoorState
+; turn key event k into an action on state s
+(check-expect (door-actions LOCKED "u") CLOSED)
+(check-expect (door-actions CLOSED "l") LOCKED)
+(check-expect (door-actions CLOSED " ") OPEN)
+(check-expect (door-actions OPEN "a") OPEN)
+(check-expect (door-actions CLOSED "a") CLOSED)
+(define (door-actions s k)
+  (cond
+    [(and (string=? LOCKED s) (string=? "u" k))
+     CLOSED]
+    [(and (string=? CLOSED s) (string=? "l" k))
+     LOCKED]
+    [(and (string=? CLOSED s) (string=? " " k))
+     OPEN]
+    [else s]))
+
+; DoorState -> Image
+; translates the state s into a large text image
+(check-expect (door-render CLOSED)
+              (text CLOSED 40 "red"))
+(define (door-render s)
+  (text s 40 "red"))
+
+; DoorState -> DoorState
+; simulates a door with an automatic door closer
+(define (door-simulation initial-state)
+  (big-bang initial-state
+    [on-tick door-closer 3]
+    [on-key door-actions]
+    [to-draw door-render]))
