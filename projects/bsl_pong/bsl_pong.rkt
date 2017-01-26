ce369c182d50ed1997b55195c6298ec6cb5c7483
antonio-carvalho <acarvalho@veniamworks.com>
2017-01-26 08:23:53 +0000

    bsl_pong: create canvas, add net and player 1 Player 1 is placed on the field and moved up and down when the "up" and "down" keys are pressed. When two keys are pressed simultaneously only the last key pressed has effect. Open issues: - How to place and move both player cursors simultaneously? - How to define the interactions between the ball and the players? Should I have a structure working as a global universe state?

--- projects/bsl_pong/bsl_pong.rkt
+++ projects/bsl_pong/bsl_pong.rkt
@@ -8,25 +8,106 @@
 ; - a net;
 ; - a score board (one number on each side of the field)
 ; - a ball
-(require "lib/image")
+(require 2htdp/image)
+(require 2htdp/universe)
 
 (define FIELD-HEIGHT 200)
-(define FIEL-WIDTH 20)
-(define FIELD (empty-scene FIELD-WIDTH FIELD-HEIGHT))
-(define PLAYER-HEIGHT (/ FIELD-HEIGHT 10))
-(define PLAYER-WIDHT (/ FIELD-WIDTH 100))
+(define FIELD-WIDTH (* FIELD-HEIGHT 2))
+(define CANVAS (empty-scene FIELD-WIDTH FIELD-HEIGHT))
+(define PLAYER-HEIGHT (/ FIELD-HEIGHT 8))
+(define PLAYER-WIDTH (/ FIELD-WIDTH 80))
+(define PLAYER-SPEED 3)
 (define PLAYER (rectangle PLAYER-WIDTH PLAYER-HEIGHT "solid" "black"))
+(define FIELD (add-line CANVAS (/ FIELD-WIDTH 2) 0 (/ FIELD-WIDTH 2) FIELD-HEIGHT (pen "black" PLAYER-WIDTH "short-dash" "butt" "bevel")))
 
-(define-struct player [location dir score])
+(define-struct player [pos score])
 ; Player is a structure
 ; (make-player Posn Number Score)
-; interpretation information regarding a player. location stores the x and y
+; interpretation information regarding a player. pos stores the x and y
 ; coordinates of where the player is, dir is the direction (-1 if going up,
 ; 1 if going down) the player is heading and score is a non negative number,
 ; starting at 0, that represents the player's score
 
-(define-struct ball [location velocity])
+(define-struct ball [pos vel])
 ; Ball is a structure
 ; (make-ball Posn Number)
-; interpretation location represents where the ball is (it's x and y coordinates)
-; and velocity is an Integer for the direction and speed of the ball 
+; interpretation pos represents where the ball is (it's x and y coordinates)
+; and vel is an Integer for the direction and speed of the ball
+
+(define example1 (make-player (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (/ FIELD-HEIGHT 2)) 0))
+(define example2 (make-player (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) 0) 0))
+(define example3 (make-player (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) FIELD-HEIGHT) 0))
+
+(check-expect (move-player1 example1 "up")
+              (make-player
+               (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (- (/ FIELD-HEIGHT 2) PLAYER-SPEED))
+               0))
+(check-expect (move-player1 example1 "down")
+              (make-player
+               (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (+ (/ FIELD-HEIGHT 2) PLAYER-SPEED))
+               0))
+(check-expect (move-player1 example2 "up")
+              (make-player
+               (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (/ PLAYER-HEIGHT 2))
+               0))
+(check-expect (move-player1 example3 "down")
+              (make-player
+               (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (- FIELD-HEIGHT (/ PLAYER-HEIGHT 2)))
+               0))
+; Player KeyEvent -> Player
+; move player up and down the y axis when the "up" and "down" key's are pressed,
+; accordingly
+(define (move-player1 p ke)
+  (cond
+    [(> (posn-y (player-pos p)) (- FIELD-HEIGHT (/ PLAYER-HEIGHT 2)))
+     (make-player
+     (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (- FIELD-HEIGHT (/ PLAYER-HEIGHT 2)))
+     (player-score p)) ]
+    [(< (posn-y (player-pos p)) (/ PLAYER-HEIGHT 2))
+     (make-player
+     (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (/ PLAYER-HEIGHT 2))
+     (player-score p))]
+    [(string=? ke "up")
+    (make-player
+     (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (- (posn-y (player-pos p)) PLAYER-SPEED))
+     (player-score p))]
+    [(string=? ke "down")
+    (make-player
+     (make-posn (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (+ (posn-y (player-pos p)) PLAYER-SPEED))
+     (player-score p))]
+    [else p]
+    ))
+
+; Player KeyEvent -> Player
+; move player up and down the y axis when the "w" and "s" key's are pressed,
+; accordingly
+(define (move-player2 p ke)
+  (cond
+    [(> (posn-y (player-pos p)) (- FIELD-HEIGHT (/ PLAYER-HEIGHT 2)))
+     (make-player
+     (make-posn (/ PLAYER-WIDTH 2) (- FIELD-HEIGHT (/ PLAYER-HEIGHT 2)))
+     (player-score p)) ]
+    [(< (posn-y (player-pos p)) (/ PLAYER-HEIGHT 2))
+     (make-player
+     (make-posn (/ PLAYER-WIDTH 2) (/ PLAYER-HEIGHT 2))
+     (player-score p))]
+    [(string=? ke "w")
+    (make-player
+     (make-posn (/ PLAYER-WIDTH 2) (- (posn-y (player-pos p)) PLAYER-SPEED))
+     (player-score p))]
+    [(string=? ke "s")
+    (make-player
+     (make-posn (/ PLAYER-WIDTH 2) (+ (posn-y (player-pos p)) PLAYER-SPEED))
+     (player-score p))]
+    ))
+
+(check-expect (render example1)
+              (place-image PLAYER (- FIELD-WIDTH (/ PLAYER-WIDTH 2)) (/ FIELD-HEIGHT 2) FIELD))
+; Player -> Image
+(define (render p)
+  (place-image PLAYER (posn-x (player-pos p)) (posn-y (player-pos p)) FIELD))
+
+(define (pong p)
+  (big-bang p
+   [to-draw render]
+   [on-key move-player1]))
