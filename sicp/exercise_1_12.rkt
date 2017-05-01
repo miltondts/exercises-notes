240621bc9e7703c9153b3393c6f8fb5c46e756a4
antonio-carvalho <acarvalho@veniamworks.com>
2017-05-01 12:26:45 +0100

    sicp: Make pascal triangle more readable

--- sicp/exercise_1_12.rkt
+++ sicp/exercise_1_12.rkt
@@ -3,27 +3,27 @@
 ; Exercise 1.12
 
 ; Number Number -> Number
-; Get a given number from the Pascal Triangle from row n and position p
-(define (pascal-pos n p)
+; Get a given number from the Pascal Triangle from row row and column col
+(define (pascal-pos row col)
     (cond
-      ((= p 0) 1)
-      ((= p n) 1)
+      ((> col row) #f)
+      ((or (= col 0) (= col row)) 1)
       (else
-       (+ (pascal-pos (- n 1) (- p 1)) (pascal-pos (- n 1) p)))))
+       (+ (pascal-pos (- row 1) (- col 1)) (pascal-pos (- row 1) col)))))
 
 ; Number Number -> List
 ; Given the number and an index of the row, return a list of numbers in that row, up to that index
-(define (row-list n p)
-    (if (= p 0)
-      (cons (pascal-pos n 0) '())
-      (cons (pascal-pos n p) (row-list n (- p 1)))))
+(define (row-list row col)
+    (if (= col 0)
+      (cons (pascal-pos row 0) '())
+      (cons (pascal-pos row col) (row-list row (- col 1)))))
 
 ; Number -> List
 ; Given the number of the row, return a list with the numbers within than row
-(define (pascal-row n)
-  (row-list n n))
+(define (pascal-row row)
+  (row-list row row))
 
-(define (pascal-triangle n)
+(define (pascal-triangle row)
   (do ((i 0 (+ i 1)))
-    ((= i n) (display (pascal-row i)))  
+    ((= i row) (display (pascal-row i)))  
   (display (pascal-row i)) (newline)))
