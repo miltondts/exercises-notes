b3e64444229736bbc6035f527dd987b8755fbcec
antonio-carvalho <acarvalho@veniamworks.com>
2017-05-10 08:23:16 +0100

    sicp: Got the transformation, ain't got the functions yet

---
+++ sicp/excercise_1_19.rkt
@@ -0,0 +1,26 @@
+#lang scheme
+
+(define (even? a)
+  (= (remainder a 2) 0))
+
+(define (square a)
+  (* a a))
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (square p) (square q))
+                   (+ (square q) (* 2 q p))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b q) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+                   
+          
