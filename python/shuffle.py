69abd252f09fa346598d6487384e9d2f818452a9
antonio-carvalho <acarvalho@veniamworks.com>
2018-02-25 16:25:22 +0000

    python: Account for the time each algorith takes

--- python/shuffle.py
+++ python/shuffle.py
@@ -1,24 +1,25 @@
 #!/usr/sbin/python
 #TODO:
 # Add tests
-# Test how long each algorithm takes
 
 import itertools
 import random
+import time
 
 N = 10000
-deck = ['a', 'b', 'c']
+deck = list('abc') 
 
 def get_occurences(ndecks, pattern):
 	return len(list(filter(lambda x : x == pattern, ndecks)))
 	
 def nshuffle(n, shuffle, deck):
+	t = time.time()
 	decks = []
 	for i in range(n):
 		tmp = shuffle(deck[:])
 		decks.append(tuple(tmp))
-
-	return decks
+	elapsed = time.time() - t
+	return decks, elapsed
 
 def good_shuffle(deck):
  	N = len(deck)
@@ -37,13 +38,35 @@ def bad_shuffle(deck):
 
 	return deck
 
+def shuffle2(deck):
+	N = len(deck)
+	swapped = [False] * N
+	while not all(swapped):
+		i, j = random.randrange(N), random.randrange(N)
+		swapped[i] = True
+		swap(deck, i, j)
+
+	return deck
+
+def shuffle3(deck):
+ 	N = len(deck)
+	for i in range(N):
+		swap(deck, i, random.randrange(i, N))
+
+	return deck
+
 def swap(deck, i, j):
 	deck[i], deck[j] = deck[j], deck[i]
 
-algorithms = {"BAD SHUFFLE": bad_shuffle, "GOOD SHUFFLE": good_shuffle}
+algorithms = {
+	"BAD SHUFFLE": bad_shuffle, 
+	"GOOD SHUFFLE": good_shuffle,
+	"SHUFFLE 2": shuffle2,
+	"SHUFFLE 3": shuffle3,
+	}
 perm = list(itertools.permutations(deck))
 for n in algorithms:
-	ndecks = nshuffle(N, algorithms[n], deck)
-	print n
+	ndecks, t = nshuffle(N, algorithms[n], deck)
+	print '{}: {} seconds'.format(n, t)
 	for i in range(len(perm)):
-		print (perm[i], get_occurences(ndecks,perm[i]) * 100 / N)
+		print (perm[i], get_occurences(ndecks, perm[i]) * 100 / N)
