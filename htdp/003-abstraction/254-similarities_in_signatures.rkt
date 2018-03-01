73a25e1f20343c0ed124724efe9e826d2e22cd58
antonio-carvalho <acarvalho@veniamworks.com>
2018-03-01 09:00:02 +0000

    htdp: Designing Abstractions - 254:255
    single points of control rule! \m/

---
+++ htdp/003-abstraction/254-similarities_in_signatures.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname 254-similarities_in_signatures) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 254
+
+; [List-of Number] [Number Number -> Boolean] -> [List-of Number]
+; (define (sort-n ...) ...)
+
+; [List-of String] [String String -> Boolean] -> [List-of String]
+; (define (sort-s ...) ...)
+
+; [X Y] [List-of X] [X X -> Y] -> [List-of X]
+
+; [List-of IR] [IR IR -> Boolean] -> [List-of IR]
+; (define (sort-IR ...) ...)
---
+++ htdp/003-abstraction/255-similarities_in_signatures.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname 255-similarities_in_signatures) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 255
+
+; [List-of Number] [Number -> Number] -> [List-of Number]
+; (define (map-n ...) ...)
+
+; [List-of String] [String -> String] -> [List-of String]
+; (define (map-s ...) ...)
+
+; [List-of X] [X -> X] -> [List-of X]
+
+; [List-of IR] [IR -> IR] -> [List-of IR]
+; (define (map-IR ...) ...)
